162e13422911a52f443a260e818328b27f1754e3
Aaron Karp <aaronkarp123@gmail.com>
2019-02-21 14:58:07 -0500

    Updated models trained with 5-200 classes

--- code/lstmHelpers.py
+++ code/lstmHelpers.py
@@ -1,4 +1,5 @@
 from helpers import *
+from pyfftw.interfaces.numpy_fft import rfft, irfft
 
 def load_classes(filedir, num_classes, display=False):
     ## Find random classes proportional to the ratio of used_files to unused_files
@@ -36,9 +37,9 @@ def generate_data_file(name, filedir, used_classes, unused_classes, order_to_use
     ## Load all data from given classes in "order_to_use" and write to file, 
     ## dicatated by "name"
     
-    used_f = open(filedir + '/sampledFiles.txt', "r")
+    used_f = open('../sampledFiles.txt', "r")
     used_files = used_f.read().split('\n')
-    unused_f = open(filedir + '/unsampledFiles.txt', "r")
+    unused_f = open('../unsampledFiles.txt', "r")
     unused_files = unused_f.read().split('\n')
     
     used_length = len(used_classes)
@@ -55,7 +56,6 @@ def generate_data_file(name, filedir, used_classes, unused_classes, order_to_use
             tempname = used_files[used_classes[to_use_index]]
             if tempname not in used_dict:
                 used_dict[tempname] = order_to_use[i]
-    
     open(filedir + name, 'w').close()  # Delete previous info
    
     with open(filedir + name, "a") as training_file:
@@ -64,11 +64,13 @@ def generate_data_file(name, filedir, used_classes, unused_classes, order_to_use
             if to_use_index >= used_length:
                 if match_matrix[unused_classes[to_use_index - used_length]][2] in used_dict:  # if you match to a used file in the current data set
                     to_use_index = used_dict[match_matrix[unused_classes[to_use_index - used_length]][2]]
-                info = generate_matched_stream(unused_files, match_matrix, unused_classes[to_use_index - used_length], 
-                                               to_use_index)  # Reset counter for second array
+                    info = generate_matched_to_used_stream(unused_files, used_files, order_to_use[i] - used_length, to_use_index, match_matrix[unused_classes[order_to_use[i] - used_length]][2])
+                else:
+                    info = generate_matched_stream(unused_files, match_matrix, unused_classes[to_use_index - used_length],
+                                                   to_use_index)  # Reset counter for second array
             else:
                 info = generate_used_stream(used_files, used_classes[to_use_index], to_use_index)
-            to_write = info[0] + " -_- " + info[1] + " -_- " + info[2] + " -_- " + str(random.randint(0, max_ramp_length)) + " -_- " + str(random.randint(0, max_ramp_length)) + " -_- " + str(random.randint(0, 600) / 10.0) + " -_- " + str(np.random.normal(1, 0.1, 1)[0]) + "\n"
+            to_write = info[0] + " -_- " + info[1] + " -_- " + info[2] + " -_- " + str(random.randint(0, max_ramp_length)) + " -_- " + str(random.randint(0, max_ramp_length)) + " -_- " + str(random.randint(100, 600) / 10.0) + " -_- " + str(np.random.normal(1, 0.1, 1)[0]) + "\n"
             training_file.write(to_write)
 
 def load_stream(name, filedir='../'):
@@ -101,6 +103,10 @@ def generate_matched_stream(unused_files, match_matrix, class_to_use, to_use):
     to_check = [unused_files[class_to_use], str(to_use), match_matrix[class_to_use][2]]
     return to_check
 
+def generate_matched_to_used_stream(unused_files, used_files, file_number, used_file_number, matched_class):
+    to_check = [unused_files[file_number], str(matched_class), used_files[used_file_number]]
+    return to_check
+
 def apply_ramp(y, fadein_len = 0, fadeout_len = 0):
     ## Create a linear ramp for fading in and out
     ## Apply the two fades to "y" and return
@@ -130,8 +136,11 @@ def apply_noise(y, target_snr_db=20):
     # Generate an sample of white noise
     mean_noise = 0
     noise_volts = np.random.normal(mean_noise, noise_avg_watts, len(x_watts))
+    Xb = rfft(noise_volts) / len(noise_volts)
+    Sb = np.arange(Xb.size)+1  # Filter
+    yb = irfft(Xb/Sb).real[:len(noise_volts)]
     # Noise up the original signal
-    noisy_signal = y + noise_volts
+    noisy_signal = y + yb
 
     return np.clip(noisy_signal, -1, 1)
 
